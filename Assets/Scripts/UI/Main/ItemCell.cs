4cdb05e8ac547bc41d39afdf086697f4dda0e79c
ftvoid <git-user@ftvoid.com>
2020-02-01 13:13:44 +0900

    Test用のシーン追加

---
+++ Assets/Scripts/UI/Main/ItemCell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCell : MonoBehaviour
+{
+
+    [SerializeField] private Image image;
+
+    public void ImageUpdate(Sprite _sprite)
+    {
+        image.sprite = _sprite;
+    }
+
+
+}
---
+++ Assets/Scripts/UI/Main/MainUITest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class MainUITest : MonoBehaviour
+{
+
+    public FloatReactiveProperty time = new FloatReactiveProperty(0);
+    public IntReactiveProperty scorePoint = new IntReactiveProperty(0);
+    public float staminaMax = 100;
+    public float staminaConsumption = 100;
+    public float staminaRecovery = 70;
+    public FloatReactiveProperty staminaNow = new FloatReactiveProperty(0);
+
+    void Start()
+    {
+        staminaNow.Value = staminaMax;
+    }
+
+    void Update ()
+    {
+        
+        time.Value -= Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            scorePoint.Value += 10;
+        }
+
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            staminaNow.Value = Mathf.Clamp(staminaNow.Value - staminaConsumption * Time.deltaTime, 0, staminaMax);
+        }
+        else if(staminaNow.Value < staminaMax)
+        {
+            staminaNow.Value = Mathf.Clamp(staminaNow.Value + staminaRecovery * Time.deltaTime, 0, staminaMax);
+        }
+
+    }
+}
