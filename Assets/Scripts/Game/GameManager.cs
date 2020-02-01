686063d19089826fd72a069e4d26c7c851f244ac
ftvoid <git-user@ftvoid.com>
2020-02-01 17:03:08 +0900

    ゲーム開始メソッドをマネージャーに追加。

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 /// <summary>
 /// ゲーム進行管理
 /// </summary>
-public class GameManager : MonoBehaviour
+public class GameManager : SingletonMonoBehaviour<GameManager>
 {
+    public void StartGame()
+    {
+        Debug.Log("ゲーム開始！");
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // 今は自動で開始
+        StartGame();
+    }
 }
