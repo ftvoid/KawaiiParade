acb7b007f4fc0b6b750ae757c8785e068e552084
ftvoid <git-user@ftvoid.com>
2020-02-01 18:00:47 +0900

    カウントダウンのデータ更新部分実装。

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     public void StartGame()
     {
         Debug.Log("ゲーム開始！");
+        StartCoroutine(InvokeCountDown());
     }
 
     protected override void Awake()
@@ -18,5 +19,15 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
 
         // 今は自動で開始
         StartGame();
+    }
+
+    private IEnumerator InvokeCountDown()
+    {
+        while ( GameState.Instance.RemainTime > 0 )
+        {
+            yield return new WaitForSeconds(1);
+            --GameState.Instance.RemainTime;
+            Debug.Log($"残り時間：{GameState.Instance.RemainTime}");
+        }
     }
 }
