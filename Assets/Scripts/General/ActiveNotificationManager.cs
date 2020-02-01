f9f69b9313c4e63ff16f0d18f40ced6b157ff3bb
ftvoid <git-user@ftvoid.com>
2020-02-01 17:50:26 +0900

    イベント通知クラスを追加

---
+++ Assets/Scripts/General/ActiveNotificationManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ゲームイベント
+/// </summary>
+public enum GameEventID
+{
+	None = -1,
+	SetUp,
+	CountDown,
+	Play,
+	Pause,
+	End,
+
+}
+
+public class ActiveNotificationManager : MonoBehaviour
+{
+	/// <summary>
+	/// シングルトン
+	/// </summary>
+	public static ActiveNotificationManager instance;
+
+	public GameEventID GetEventID
+	{
+		get;
+		private set;
+	}
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
+
+	/// <summary>
+	/// ゲーム内の通知
+	/// </summary>
+	/// <param name="id"> イベントID </param>
+	public void Notification(GameEventID id)
+	{
+		GetEventID = id;
+	}
+
+}
---
+++ Assets/Scripts/General/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class Receiver : MonoBehaviour
+{
+	/// <summary>
+	/// コールバック
+	/// </summary>
+	private ReactiveProperty<GameEventID> m_event_callback = new ReactiveProperty<GameEventID>();
+	public ReactiveProperty<GameEventID> EventCallBack
+	{
+		get { return m_event_callback; }
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		//メッセージ受信
+		this.UpdateAsObservable()
+			.Select(_ => ActiveNotificationManager.instance.GetEventID)
+			.DistinctUntilChanged()
+			.Subscribe(x => m_event_callback.Value = x);
+	}
+
+}
