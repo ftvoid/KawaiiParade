d80b2bbf3d64d3f58f0608570b5e48c6038a146a
ftvoid <git-user@ftvoid.com>
2020-02-01 17:31:10 +0900

    Titleシーンの作成、チュートリアルに画像を差し替え

---
+++ Assets/Scripts/UI/Title/TitleFadeView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TitleFadeView : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup panel;
+    [SerializeField] private float fadeTime = 1;
+    public void Fade(int _fadeAlpha) 
+    {
+        panel.DOFade(_fadeAlpha, fadeTime);
+    }
+}
