00ec1e9b0cb6ce09a25cdcd53d40ab9f066ac253
ftvoid <git-user@ftvoid.com>
2020-02-01 20:14:31 +0900

    サウンドマネージャ追加。

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : SingletonMonoBehaviour<SoundManager>
+{
+    private SoundParameter _param;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _param = Resources.Load<SoundParameter>("ScriptableObjects/SoundParameter");
+
+        Debug.Log(_param);
+    }
+}
