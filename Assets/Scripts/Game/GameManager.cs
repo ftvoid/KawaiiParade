74fee274981d420737daab8508a388db02c4c37f
ftvoid <git-user@ftvoid.com>
2020-02-01 11:19:47 +0900

    ゲームマネージャー追加。

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲーム進行管理
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+}
---
+++ Assets/Scripts/Game/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲームの状態
+/// </summary>
+public class GameState : SingletonMonoBehaviour<GameState>
+{
+    /// <summary>
+    /// プレイヤーが移動できる範囲
+    /// </summary>
+    [SerializeField] private Rect _movableArea;
+
+    public static Rect MovableArea => instance._movableArea;
+}
