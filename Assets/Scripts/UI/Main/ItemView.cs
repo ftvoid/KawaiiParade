e33488904e48e1293e262bc2389c5668bb76a639
ftvoid <git-user@ftvoid.com>
2020-02-01 15:04:06 +0900

    GameシーンにCanvasを追加

--- Assets/Scripts/UI/Main/ItemView.cs
+++ Assets/Scripts/UI/Main/ItemView.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public List<Image> m_itemCell = new List<Image>();
+
+
+    public void ItemUpdate()
     {
-        
+
     }
 }
