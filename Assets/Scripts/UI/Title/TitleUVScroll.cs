17f68a813aa384e446f344eddc29e1630e7dae48
ftvoid <git-user@ftvoid.com>
2020-02-02 16:43:00 +0900

    Ex : Title UVスクロール

---
+++ Assets/Scripts/UI/Title/TitleUVScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleUVScroll : MonoBehaviour
+{
+    public RawImage image;
+    public float speed = 0.05f;
+    public float uv_x = 0;
+    public float uv_y = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uv_x += speed * Time.deltaTime;
+        uv_y += speed * Time.deltaTime;
+        image.uvRect = new Rect(uv_x, uv_y, 1, 1);
+    }
+}
