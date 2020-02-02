3af15fd10cb0c59ea4b62b89d8e8d6151b8cd348
ftvoid <git-user@ftvoid.com>
2020-02-02 14:36:43 +0900

    クレジットのありがとう表示を追加。

---
+++ Assets/Scripts/Credit/CreditFader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class CreditFader : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _target;
+
+    [SerializeField] private float _fadeSpeed = 1;
+
+    private void Update()
+    {
+        _target.alpha += Mathf.Clamp01(_fadeSpeed * Time.deltaTime);
+    }
+}
