735670ae79c76ff332e4b07719a36217b4806a98
ftvoid <git-user@ftvoid.com>
2020-02-02 11:02:21 +0900

    リザルトシナリオ追加。

---
+++ Assets/END/UtageStarter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utage;
+using UtageExtensions;
+
+public class UtageStarter : MonoBehaviour
+{
+    AdvEngine Engine { get { return this.GetComponentCacheFindIfMissing( ref engine); } }
+    [SerializeField]
+    AdvEngine engine;
+
+    void Start()
+    {
+        Debug.Log("conversation start");
+        StartCoroutine(CoTalk());
+    }
+
+        IEnumerator CoTalk(){
+
+        // ここでジャンプするシナリオラベルを選択
+        Engine.JumpScenario("Happy");
+
+        // 「宴」のシナリオ終了待ち
+        while(!Engine.IsEndScenario)
+        {
+            yield return null;
+        }
+
+        // シナリオ再生後はタイトルシーンへ遷移
+    }
+}
