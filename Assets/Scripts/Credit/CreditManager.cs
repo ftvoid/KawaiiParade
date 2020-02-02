fda36eba7300ca51dde300bdc28b8ade86318dfc
ftvoid <git-user@ftvoid.com>
2020-02-02 13:15:57 +0900

    クレジットシーン分岐実装。

--- Assets/Scripts/Credit/CreditManager.cs
+++ Assets/Scripts/Credit/CreditManager.cs
@@ -5,10 +5,34 @@ using UniRx.Triggers;
 
 public class CreditManager : MonoBehaviour
 {
-    private void Awake()
+    [SerializeField] private GameObject _happyEnding;
+
+    [SerializeField] private GameObject _gameoverEnding;
+
+    [SerializeField] private GameObject _goodbyeEnding;
+
+    private void Start()
     {
         this.UpdateAsObservable()
             .Where(_ => Input.anyKeyDown)
             .Subscribe(_ => SceneChanger.Instance.ChangeScene(SceneType.Title));
+
+        var param = SceneChanger.Instance.Param;
+        var senario = param != null ? (string)param : "Happy";
+
+        switch ( senario )
+        {
+            case "Happy":
+                _happyEnding.SetActive(true);
+                break;
+
+            case "Gameover":
+                _gameoverEnding.SetActive(true);
+                break;
+
+            case "Goodbye":
+                _goodbyeEnding.SetActive(true);
+                break;
+        }
     }
 }
