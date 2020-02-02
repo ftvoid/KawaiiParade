d56d0cd1404eac826d0312de76ca30e69dee06cd
ftvoid <git-user@ftvoid.com>
2020-02-02 12:15:13 +0900

    クレジットシーンからタイトルに遷移できるようにした。

---
+++ Assets/Scripts/Credit/CreditManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class CreditManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        this.UpdateAsObservable()
+            .Where(_ => Input.anyKeyDown)
+            .Subscribe(_ => SceneChanger.Instance.ChangeScene(SceneType.Title));
+    }
+}
