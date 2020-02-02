db27e91ef0c7dc7018da4e261883ab47a6322c21
ftvoid <git-user@ftvoid.com>
2020-02-02 16:13:10 +0900

    あらすじの誤字修正。

--- Assets/Scripts/UI/TextView.cs
+++ Assets/Scripts/UI/TextView.cs
@@ -19,7 +19,7 @@ public class TextView : MonoBehaviour
 	[SerializeField]
 	private float _message_speed = 20.0f;
 
-	readonly string text = "彼氏と彼女がケンカ。\n 彼氏が怒りにまかせ、彼女の私物をマンションの80階から\n地上のまき散らしてしまった！\nしかも、家を追い出されてしまう始末！\n \n ”お前のファッションセンスは全然理解できない！\nもっとかわいい女の子になってほしいんだよ！！”";
+	readonly string text = "彼氏と彼女がケンカ。\n 彼氏が怒りにまかせ、彼女の私物をマンションの80階から、\n地上にまき散らしてしまった！\nしかも、家を追い出されてしまう始末！\n \n ”お前のファッションセンスは全然理解できない！\nもっとかわいい女の子になってほしいんだよ！！”";
 
 	// Start is called before the first frame update
 	void Start()
